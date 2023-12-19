194d9dfc77b31113ef479a95b625c44310d49fe6
Aubcodes <aub388@myyahoo.com>
2023-12-19 08:34:59 +0100

    verify test

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -39,180 +39,6 @@ authRoute.get("/", authMiddleware, async (req, res) => {
   }
 });
 
-authRoute.post("/", async (req, res) => {
-  const eventData = req.body.data;
-  const eventType = req.body.event;
-
-  switch (eventType) {
-    case "charge.success":
-      // Extract relevant data from the Paystack event
-      const userEmail = eventData.customer.email;
-      const paymentInterval = eventData.plan.interval;
-
-      // Map Paystack plan intervals to your plan values
-      const planMapping = {
-        monthly: "monthly",
-        annually: "yearly",
-        // Add more mappings if needed
-      };
-
-      // Determine the subscribed plan based on the Paystack event
-      const subscribedPlan = planMapping[paymentInterval] || "free";
-
-      try {
-        const user = await User.findOne({ email: userEmail });
-
-        if (user) {
-          // Assuming your User model has a field for the user's subscribed plan
-          user.plan = subscribedPlan;
-
-          // Save the updated user to the database
-          await user.save();
-
-          console.log(`User ${userEmail} plan updated to ${subscribedPlan}`);
-
-          // Send a success response to the frontend
-          return res.status(200).json({
-            message: "Charge success and user plan updated",
-            userEmail,
-            subscribedPlan,
-          });
-        } else {
-          console.log(`User with email ${userEmail} not found`);
-          return res.status(404).json({
-            error: "User not found",
-          });
-        }
-      } catch (error) {
-        console.error("Error updating user plan:", error);
-        return res.status(500).json({
-          error: "Internal Server Error",
-        });
-      }
-
-    case "charge.dispute.create":
-      console.log("Dispute created:", eventData);
-      // Add logic for dispute creation handling
-      break;
-
-    case "charge.dispute.remind":
-      console.log("Dispute reminder:", eventData);
-      // Add logic for dispute reminder handling
-      break;
-
-    case "charge.dispute.resolve":
-      console.log("Dispute resolved:", eventData);
-      // Add logic for dispute resolution handling
-      break;
-
-    case "customeridentification.failed":
-      console.log("Customer ID validation failed:", eventData);
-      // Add logic for customer ID validation failure handling
-      break;
-
-    case "customeridentification.success":
-      console.log("Customer ID validation successful:", eventData);
-      // Add logic for customer ID validation success handling
-      break;
-
-    case "dedicatedaccount.assign.failed":
-      console.log("DVA assign failed:", eventData);
-      // Add logic for DVA assign failure handling
-      break;
-
-    case "dedicatedaccount.assign.success":
-      console.log("DVA assign success:", eventData);
-      // Add logic for DVA assign success handling
-      break;
-
-    case "invoice.create":
-      console.log("Invoice created:", eventData);
-      // Add logic for invoice creation handling
-      break;
-
-    case "invoice.payment_failed":
-      console.log("Invoice payment failed:", eventData);
-      // Add logic for invoice payment failure handling
-      break;
-
-    case "invoice.update":
-      console.log("Invoice updated:", eventData);
-      // Add logic for invoice update handling
-      break;
-
-    case "paymentrequest.pending":
-      console.log("Payment request pending:", eventData);
-      // Add logic for payment request pending handling
-      break;
-
-    case "paymentrequest.success":
-      console.log("Payment request success:", eventData);
-      // Add logic for payment request success handling
-      break;
-
-    case "refund.failed":
-      console.log("Refund failed:", eventData);
-      // Add logic for refund failure handling
-      break;
-
-    case "refund.pending":
-      console.log("Refund pending:", eventData);
-      // Add logic for refund pending handling
-      break;
-
-    case "refund.processed":
-      console.log("Refund processed:", eventData);
-      // Add logic for refund processed handling
-      break;
-
-    case "refund.processing":
-      console.log("Refund processing:", eventData);
-      // Add logic for refund processing handling
-      break;
-
-    case "subscription.create":
-      console.log("Subscription created:", eventData);
-      // Add logic for subscription creation handling
-      break;
-
-    case "subscription.disable":
-      console.log("Subscription disabled:", eventData);
-      // Add logic for subscription disable handling
-      break;
-
-    case "subscription.expiring_cards":
-      console.log("Subscription expiring cards:", eventData);
-      // Add logic for subscription expiring cards handling
-      break;
-
-    case "subscription.not_renew":
-      console.log("Subscription not renewing:", eventData);
-      // Add logic for subscription not renewing handling
-      break;
-
-    case "transfer.failed":
-      console.log("Transfer failed:", eventData);
-      // Add logic for transfer failure handling
-      break;
-
-    case "transfer.success":
-      console.log("Transfer success:", eventData);
-      // Add logic for transfer success handling
-      break;
-
-    case "transfer.reversed":
-      console.log("Transfer reversed:", eventData);
-      // Add logic for transfer reversed handling
-      break;
-
-    default:
-      console.log(`Unhandled event type: ${eventType}`);
-      // Handle unhandled event types
-      break;
-  }
-
-  res.status(200).send("Event handled successfully!");
-});
 authRoute.post("/paystack-webhook", async (req, res) => {
   const payload = JSON.stringify(req.body);
   const signature = req.headers["x-paystack-signature"];
@@ -221,7 +47,174 @@ authRoute.post("/paystack-webhook", async (req, res) => {
     // Signature is valid, process the webhook event
     const eventType = req.body.event;
 
-    console.log("Received Paystack webhook event:", eventType);
+    const eventData = req.body.data;
+
+    switch (eventType) {
+      case "charge.success":
+        // Extract relevant data from the Paystack event
+        const userEmail = eventData.customer.email;
+        const paymentInterval = eventData.plan.interval;
+
+        // Map Paystack plan intervals to your plan values
+        const planMapping = {
+          monthly: "monthly",
+          annually: "yearly",
+        };
+
+        // Determine the subscribed plan based on the Paystack event
+        const subscribedPlan = planMapping[paymentInterval] || "free";
+
+        try {
+          const user = await User.findOne({ email: userEmail });
+
+          if (user) {
+            // Assuming your User model has a field for the user's subscribed plan
+            user.plan = subscribedPlan;
+
+            // Save the updated user to the database
+            await user.save();
+
+            console.log(`User ${userEmail} plan updated to ${subscribedPlan}`);
+
+            // Send a success response to the frontend
+            return res.status(200).json({
+              message: "Charge success and user plan updated",
+              userEmail,
+              subscribedPlan,
+            });
+          } else {
+            console.log(`User with email ${userEmail} not found`);
+            return res.status(404).json({
+              error: "User not found",
+            });
+          }
+        } catch (error) {
+          console.error("Error updating user plan:", error);
+          return res.status(500).json({
+            error: "Internal Server Error",
+          });
+        }
+
+      case "charge.dispute.create":
+        console.log("Dispute created:", eventData);
+        // Add logic for dispute creation handling
+        break;
+
+      case "charge.dispute.remind":
+        console.log("Dispute reminder:", eventData);
+        // Add logic for dispute reminder handling
+        break;
+
+      case "charge.dispute.resolve":
+        console.log("Dispute resolved:", eventData);
+        // Add logic for dispute resolution handling
+        break;
+
+      case "customeridentification.failed":
+        console.log("Customer ID validation failed:", eventData);
+        // Add logic for customer ID validation failure handling
+        break;
+
+      case "customeridentification.success":
+        console.log("Customer ID validation successful:", eventData);
+        // Add logic for customer ID validation success handling
+        break;
+
+      case "dedicatedaccount.assign.failed":
+        console.log("DVA assign failed:", eventData);
+        // Add logic for DVA assign failure handling
+        break;
+
+      case "dedicatedaccount.assign.success":
+        console.log("DVA assign success:", eventData);
+        // Add logic for DVA assign success handling
+        break;
+
+      case "invoice.create":
+        console.log("Invoice created:", eventData);
+        // Add logic for invoice creation handling
+        break;
+
+      case "invoice.payment_failed":
+        console.log("Invoice payment failed:", eventData);
+        // Add logic for invoice payment failure handling
+        break;
+
+      case "invoice.update":
+        console.log("Invoice updated:", eventData);
+        // Add logic for invoice update handling
+        break;
+
+      case "paymentrequest.pending":
+        console.log("Payment request pending:", eventData);
+        // Add logic for payment request pending handling
+        break;
+
+      case "paymentrequest.success":
+        console.log("Payment request success:", eventData);
+        // Add logic for payment request success handling
+        break;
+
+      case "refund.failed":
+        console.log("Refund failed:", eventData);
+        // Add logic for refund failure handling
+        break;
+
+      case "refund.pending":
+        console.log("Refund pending:", eventData);
+        // Add logic for refund pending handling
+        break;
+
+      case "refund.processed":
+        console.log("Refund processed:", eventData);
+        // Add logic for refund processed handling
+        break;
+
+      case "refund.processing":
+        console.log("Refund processing:", eventData);
+        // Add logic for refund processing handling
+        break;
+
+      case "subscription.create":
+        console.log("Subscription created:", eventData);
+        // Add logic for subscription creation handling
+        break;
+
+      case "subscription.disable":
+        console.log("Subscription disabled:", eventData);
+        // Add logic for subscription disable handling
+        break;
+
+      case "subscription.expiring_cards":
+        console.log("Subscription expiring cards:", eventData);
+        // Add logic for subscription expiring cards handling
+        break;
+
+      case "subscription.not_renew":
+        console.log("Subscription not renewing:", eventData);
+        // Add logic for subscription not renewing handling
+        break;
+
+      case "transfer.failed":
+        console.log("Transfer failed:", eventData);
+        // Add logic for transfer failure handling
+        break;
+
+      case "transfer.success":
+        console.log("Transfer success:", eventData);
+        // Add logic for transfer success handling
+        break;
+
+      case "transfer.reversed":
+        console.log("Transfer reversed:", eventData);
+        // Add logic for transfer reversed handling
+        break;
+
+      default:
+        console.log(`Unhandled event type: ${eventType}`);
+        // Handle unhandled event types
+        break;
+    }
 
     // Add your logic to update your app based on the webhook event
   } else {
